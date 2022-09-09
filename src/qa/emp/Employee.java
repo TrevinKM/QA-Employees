6e18c0461b9c49ab35cc21ab73601008cabf5363
TrevinKM <t.mabarana@lancaster.ac.uk>
2022-09-09 16:51:43 +0100

    Added employee class

--- src/qa/emp/Employee.java
+++ src/qa/emp/Employee.java
@@ -1,4 +1,24 @@
 package qa.emp;
 
 public class Employee {
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private Integer salary;
+    private boolean isHired;
+
+    Employee(String userName, String firstName, String lastName){
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public void setHired(boolean isHired){
+        this.isHired = isHired;
+    }
+
 }
