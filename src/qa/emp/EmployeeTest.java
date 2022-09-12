ad21135d0c9c264cda4d62fa7812bf48ecbf55fa
TrevinKM <t.mabarana@lancaster.ac.uk>
2022-09-12 12:11:32 +0100

    Done the two exercise

--- src/qa/emp/EmployeeTest.java
+++ src/qa/emp/EmployeeTest.java
@@ -2,10 +2,26 @@ package qa.emp;
 
 public class EmployeeTest {
     public static void main(String[] args) {
-        int myInt = 5;
+        int myInt = 100;
+
+        int x = 100, y = 5, z = -30;
+        int result = x*y*z;
+
+        System.out.println(result);
+
+        String street = "1600 Pennsylvania Avenue NW";
+        String city = "Washington";
+        String state = "DC";
+        int zip = 20500;
+        String whitehouseAddress = street + city + state + zip;
+        System.out.println(whitehouseAddress);
+
+
         char myChar = 'c';
 
 
         System.out.println("Employee details: " + myInt + myChar);
+
+
     }
 }
